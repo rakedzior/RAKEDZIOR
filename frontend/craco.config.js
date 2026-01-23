6c24baa6ab20504e60458a83ae35d6472a4342e3
rakedzior <ra.kedzior@gmail.com>
2026-01-23 11:07:16 +0000

    Fix build and add lockfile for CI

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -35,7 +35,6 @@ if (config.enableHealthCheck) {
 const webpackConfig = {
   eslint: {
     configure: {
-      extends: ["plugin:react-hooks/recommended"],
       rules: {
         "react-hooks/rules-of-hooks": "error",
         "react-hooks/exhaustive-deps": "warn",
